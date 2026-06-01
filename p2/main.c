f2f63862ec2eb35509159a2b312e4e3e2685be19
2025020135 <ssuhyeon@kau.kr>
2026-06-01 15:26:10 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -3,19 +3,21 @@
 int main(void) {
     int N;
     int a[20], b[20];
+    int *p = nullptr;
+    int *q = nullptr;
 
     scanf("%d", &N);
 
-    for (int *p = a; p < a + N; p++) {
+    for (p = a; p < a + N; p++) {
         scanf("%d", p);
     }
 
-    for (int *q = b; q < b + N; q++) {
+    for (q = b; q < b + N; q++) {
         scanf("%d", q);
     }
 
-    int *p = a;
-    int *q = b + N - 1;
+    p = a;
+    q = b + N - 1;
 
     while (p < a + N) {
         printf(" %d", *p + *q);
